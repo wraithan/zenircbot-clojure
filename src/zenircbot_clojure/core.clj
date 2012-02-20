5c9c5f4cbc212bab6c6bc771ddffe341f57ab41f
Chris McDonald <XWraithanX@gmail.com>
2012-02-19 21:26:15 -1000

    initialized with lein

---
+++ src/zenircbot_clojure/core.clj
@@ -0,0 +1,1 @@
+(ns zenircbot-clojure.core)
---
+++ test/zenircbot_clojure/test/core.clj
@@ -0,0 +1,6 @@
+(ns zenircbot-clojure.test.core
+  (:use [zenircbot-clojure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

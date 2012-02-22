693636d50683142a81144e56b2ae0f51e52ef2a4
Chris McDonald <XWraithanX@gmail.com>
2012-02-21 20:22:12 -0800

    first pass, crashes when you send something

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
-(defproject zenircbot-clojure "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject zenircbot-clojure "1.0.0"
+  :description "The clojure version of ZenIRCBot written as an
+  experiment for the author."
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [irclj "0.4.1"]
+                 [clj-redis "0.0.12"]
+                 [cheshire "2.2.0"]])
--- src/zenircbot_clojure/core.clj
+++ src/zenircbot_clojure/core.clj
@@ -1,1 +1,31 @@
-(ns zenircbot-clojure.core)
+(ns zenircbot-clojure.core
+  (:require [clj-redis.client :as redis])
+  (:use [irclj.core]
+        [cheshire.core]))
+
+(def db (redis/init))
+
+(def fnmap {:on-message (fn [{:keys [nick channel message irc]}]
+                          (redis/publish db "in" (generate-string
+                                                  {:version 1
+                                                   :type "privmsg"
+                                                   :data {:message message
+                                                          :channel channel
+                                                          :sender nick}})))})
+
+(def bot (connect (create-irc {:name "ZenIRCBot-clojure"
+                               :server "irc.freenode.net"
+                               :fnmap fnmap})
+                  :channels ["#pdxbots"]))
+
+(redis/subscribe db ["out"] (fn [ch msg]
+                              (let [message (parse-string msg true)]
+                                ((println "say "
+                                          ((message :data) :message)
+                                          " to "
+                                          ((message :data) :to))
+                                 (send-message bot
+                                               ((message :data) :to)
+                                               ((message :data) :message))))))
+(read-line)
+(close bot)

1df60868609991b77fbc7cf55e4fd079aa947ab6
Chris McDonald <XWraithanX@gmail.com>
2012-02-22 17:39:36 -0800

    It's alive\!

--- src/zenircbot_clojure/core.clj
+++ src/zenircbot_clojure/core.clj
@@ -3,29 +3,30 @@
   (:use [irclj.core]
         [cheshire.core]))
 
-(def db (redis/init))
+(def pub (redis/init))
+(def sub (redis/init))
 
-(def fnmap {:on-message (fn [{:keys [nick channel message irc]}]
-                          (redis/publish db "in" (generate-string
-                                                  {:version 1
-                                                   :type "privmsg"
-                                                   :data {:message message
-                                                          :channel channel
-                                                          :sender nick}})))})
+(def bot-fnmap {:on-message (fn [{:keys [nick channel message irc]}]
+                              (redis/publish pub "in" (generate-string
+                                                       {:version 1
+                                                        :type "privmsg"
+                                                        :data {:message message
+                                                               :channel channel
+                                                               :sender nick}})))})
 
 (def bot (connect (create-irc {:name "ZenIRCBot-clojure"
                                :server "irc.freenode.net"
-                               :fnmap fnmap})
+                               :fnmap bot-fnmap})
                   :channels ["#pdxbots"]))
 
-(redis/subscribe db ["out"] (fn [ch msg]
-                              (let [message (parse-string msg true)]
-                                ((println "say "
-                                          ((message :data) :message)
-                                          " to "
-                                          ((message :data) :to))
-                                 (send-message bot
-                                               ((message :data) :to)
-                                               ((message :data) :message))))))
+(redis/subscribe sub ["out"]
+                 (fn [ch json-msg]
+                   (let [message (parse-string json-msg true)]
+                     (if (and (= (message :version) 1)
+                              (= (message :type) "privmsg"))
+                       (send-message bot
+                                     ((message :data) :to)
+                                     ((message :data) :message))))))
+
 (read-line)
 (close bot)
